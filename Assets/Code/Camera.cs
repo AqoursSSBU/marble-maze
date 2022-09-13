96f3ff6662ae471ffc6453b4e41ccb772ce9d807
Nick Pham <hebenzudoaa@gmail.com>
2022-09-13 14:23:21 -0400

    tutorial

--- Assets/Code/Camera.cs
+++ Assets/Code/Camera.cs
@@ -6,14 +6,39 @@ public class Camera : MonoBehaviour
 {
     Transform playerTrans;
     Vector3 offSet;
+    Vector3 eulerAngles;
+    float turnSpeed = 45;
+    float x = 0,y = 0,z = 0;
 
     void Start() {
         playerTrans = GameObject.FindGameObjectWithTag("Player").transform;
-        offSet = transform.position -playerTrans.position;
+        offSet = transform.position - playerTrans.position;
     }
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = playerTrans.position + offSet;
+        
+        if(Input.GetKey(KeyCode.W)){
+            x-=1;
+        }
+        if(Input.GetKey(KeyCode.S)){
+            x+=1;
+        }
+        if(Input.GetKey(KeyCode.A)){
+            y-=1;
+        }
+        if(Input.GetKey(KeyCode.D)){
+            y+=1;
+        }
+        eulerAngles += new Vector3(x,y,z) * Time.deltaTime * turnSpeed;
+        transform.position = playerTrans.position - transform.forward*offSet.magnitude;
+        transform.localEulerAngles=eulerAngles;
+        x = 0;
+        y = 0;
+        z = 0;
+
+
+        
+        
     }
 }
--- Assets/Code/PlayerMove.cs
+++ Assets/Code/PlayerMove.cs
@@ -6,21 +6,46 @@ using UnityEngine.SceneManagement;
 public class PlayerMove : MonoBehaviour
 {
 
-    public float speed = 10, death = -20;
+    public float speed = 10, death = -20, zspeed = 0, xspeed = 0;
     Rigidbody _rigidbody;
+    Transform camtrans;
+    Vector3 forward,right;
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        camtrans = GameObject.FindGameObjectWithTag("MainCamera").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float zspeed = Input.GetAxis("Vertical") * speed;
-        float xspeed = Input.GetAxis("Horizontal") * speed;
-        _rigidbody.AddForce(new Vector3(xspeed,0,zspeed));
-
+        zspeed = 0;
+        xspeed = 0;
+        forward=camtrans.forward;
+        right=camtrans.right;
+        forward.y=0;
+        right.y=0;
+        // float zspeed = Input.GetAxis("Vertical") * speed;
+        // float xspeed = Input.GetAxis("Horizontal") * speed;
+        if(Input.GetKey(KeyCode.UpArrow)){
+            xspeed+=speed;
+        }
+        if(Input.GetKey(KeyCode.DownArrow)){
+            xspeed-=speed;
+        }
+        if(Input.GetKey(KeyCode.LeftArrow)){
+            zspeed-=speed;
+        }
+        if(Input.GetKey(KeyCode.RightArrow)){
+            zspeed+=speed;
+        }
+        
+        _rigidbody.AddForce(xspeed*forward);
+        _rigidbody.AddForce(zspeed*right);
+        print(Vector3.RotateTowards(new Vector3(xspeed,0,zspeed),camtrans.forward,8,8));
+        print(xspeed);
+        print(zspeed);
         if(transform.position.y < death){
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
